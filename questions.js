49d8d6e7dc48d017fcdf1a15d330af49890d9a81
sheremet06y-afk <sheremet06y@gmail.com>
2026-05-17 23:43:49 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -12,12 +12,34 @@ correctAnswer: 0
 },
 
 {
+question: "S čím je spojený vznik trhu?",
+options: [
+"S vysokým stupňom deľby práce",
+"S rastom inflácie",
+"So vznikom bánk",
+"So znížením výroby"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je deľba práce?",
+options: [
+"Špecializácia pracovných činností",
+"Rozdelenie zisku",
+"Forma dane",
+"Typ trhu"
+],
+correctAnswer: 0
+},
+
+{
 question: "Čo je barter?",
 options: [
 "Výmena výrobku za výrobok",
-"Typ dane",
-"Forma inflácie",
-"Bankový produkt"
+"Forma úveru",
+"Bankový produkt",
+"Daňový systém"
 ],
 correctAnswer: 0
 },
@@ -25,15 +47,670 @@ correctAnswer: 0
 {
 question: "Ktorá funkcia trhu rozmiestňuje výrobné faktory?",
 options: [
+"Alokačná",
 "Stimulačná",
 "Integračná",
-"Alokačná",
 "Kontrolná"
 ],
-correctAnswer: 2
+correctAnswer: 0
 },
 
 {
+question: "Ktorá funkcia trhu motivuje efektívnu výrobu?",
+options: [
+"Stimulačná",
+"Alokačná",
+"Integračná",
+"Distribučná"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je svetový trh?",
+options: [
+"Trh prekračujúci hranice štátov",
+"Regionálny trh",
+"Miestny trh",
+"Trh jedného mesta"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je voľný trh?",
+options: [
+"Trh bez zásahov štátu",
+"Trh regulovaný štátom",
+"Finančný trh",
+"Trh práce"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je viazaný trh?",
+options: [
+"Regulovaný trh",
+"Voľný trh",
+"Trh práce",
+"Medzinárodný trh"
+],
+correctAnswer: 0
+},
+
+{
+question: "Ktoré sú hlavné subjekty trhu?",
+options: [
+"Domácnosti, firmy, štát",
+"Banky a poisťovne",
+"Exportéri a importéri",
+"Výrobcovia a zákazníci"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je pozitívna externalita?",
+options: [
+"Prenos užitočných efektov na iné subjekty",
+"Rast cien",
+"Pokles výroby",
+"Vyššie dane"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je negatívna externalita?",
+options: [
+"Dodatočné náklady spôsobené inými subjektmi",
+"Vyšší export",
+"Rast produktivity",
+"Pokles cien"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je agregátny dopyt?",
+options: [
+"Celkové výdavky ekonomiky",
+"Objem výroby",
+"Objem exportu",
+"Daňové príjmy"
+],
+correctAnswer: 0
+},
+
+{
+question: "Ktoré zložky tvorí agregátny dopyt?",
+options: [
+"C + I + G + NX",
+"C + T + M",
+"G + X + T",
+"I + S + D"
+],
+correctAnswer: 0
+},
+
+{
+question: "Aký priebeh má krivka agregátneho dopytu?",
+options: [
+"Klesajúci",
+"Rastúci",
+"Vertikálny",
+"Nemenný"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je agregátna ponuka?",
+options: [
+"Objem produkcie podnikov",
+"Objem exportu",
+"Objem daní",
+"Objem peňazí"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je potenciálny produkt?",
+options: [
+"Produkt pri plnom využití zdrojov",
+"Objem exportu",
+"Čistý zisk podniku",
+"Daňový príjem"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je reálny produkt?",
+options: [
+"Skutočný objem produkcie ekonomiky",
+"Potenciálny produkt",
+"Objem exportu",
+"Daňový základ"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je recesná medzera?",
+options: [
+"Reálny produkt je nižší ako potenciálny",
+"Reálny produkt je vyšší",
+"Rast cien",
+"Deflácia"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je inflačná medzera?",
+options: [
+"Reálny produkt je nad potenciálom",
+"Pokles výroby",
+"Deflácia",
+"Pokles exportu"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je makroekonomická rovnováha?",
+options: [
+"AD sa rovná AS",
+"Rast exportu",
+"Pokles cien",
+"Vyrovnaný rozpočet"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo znamená HDP?",
+options: [
+"Hrubý domáci produkt",
+"Hrubý dôchodkový produkt",
+"Hospodársky produkt",
+"Hrubý obchodný produkt"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo sa započítava do HDP?",
+options: [
+"Finálne statky a služby",
+"Použité výrobky",
+"Transferové platby",
+"Medziprodukty"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je medziprodukt?",
+options: [
+"Produkt použitý pri ďalšej výrobe",
+"Konečný produkt",
+"Luxusný tovar",
+"Exportný produkt"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je finálny statok?",
+options: [
+"Konečný produkt určený na spotrebu",
+"Surovina",
+"Polotovar",
+"Výrobný faktor"
+],
+correctAnswer: 0
+},
+
+{
+question: "Ktoré metódy merania HDP poznáme?",
+options: [
+"Produkčná, výdavková, dôchodková",
+"Daňová, colná, finančná",
+"Importná, exportná, obchodná",
+"Výrobná, mzdová, úverová"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je disponibilný dôchodok?",
+options: [
+"Príjem po odpočítaní priamych daní",
+"Zisk podniku",
+"Daňový základ",
+"Výška exportu"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo znamená HNP?",
+options: [
+"Hrubý národný produkt",
+"Hospodársky nákladový produkt",
+"Hrubý obchodný produkt",
+"Hospodársky národný príjem"
+],
+correctAnswer: 0
+},
+
+{
+question: "Aký je rozdiel medzi HDP a HNP?",
+options: [
+"HDP sleduje územie, HNP národné výrobné faktory",
+"HDP zahŕňa iba služby",
+"HNP je vždy väčší",
+"Nie je rozdiel"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je inflácia?",
+options: [
+"Dlhodobý rast cenovej hladiny",
+"Pokles výroby",
+"Pokles exportu",
+"Pokles miezd"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je deflácia?",
+options: [
+"Pokles cenovej hladiny",
+"Rast cien",
+"Rast výroby",
+"Rast exportu"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je manažment?",
+options: [
+"Proces riadenia organizácie",
+"Finančný trh",
+"Forma dane",
+"Výrobný faktor"
+],
+correctAnswer: 0
+},
+
+{
+question: "V akých významoch možno chápať manažment?",
+options: [
+"Proces, profesia, vedná disciplína",
+"Export, import, inflácia",
+"Výroba, predaj, marketing",
+"Plánovanie, export, audit"
+],
+correctAnswer: 0
+},
+
+{
+question: "Kto vytvoril byrokratický prístup?",
+options: [
+"Max Weber",
+"Taylor",
+"Ford",
+"Drucker"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je taylorizmus?",
+options: [
+"Vedecké riadenie práce",
+"Forma marketingu",
+"Druh konkurencie",
+"Daňový systém"
+],
+correctAnswer: 0
+},
+
+{
+question: "Na čo sa zameriava behavioristická teória manažmentu?",
+options: [
+"Psychologicko-sociálnu stránku",
+"Daňový systém",
+"Export",
+"Infláciu"
+],
+correctAnswer: 0
+},
+
+{
+question: "Kto bol známy empirický manažér?",
+options: [
+"P. F. Drucker",
+"Ford",
+"Weber",
+"Taylor"
+],
+correctAnswer: 0
+},
+
+{
+question: "Ktorá funkcia manažmentu stanovuje ciele?",
+options: [
+"Plánovanie",
+"Kontrola",
+"Delegovanie",
+"Motivovanie"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je organizovanie?",
+options: [
+"Rozdeľovanie úloh a vzťahov",
+"Predaj výrobkov",
+"Výpočet daní",
+"Marketing"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je kontrola v manažmente?",
+options: [
+"Porovnanie plánovaného a skutočného stavu",
+"Predaj výrobkov",
+"Výroba tovaru",
+"Daňová kontrola"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo znamená SMART cieľ?",
+options: [
+"Presne definovaný a merateľný cieľ",
+"Náhodný cieľ",
+"Krátkodobý cieľ",
+"Finančný plán"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je benchmarking?",
+options: [
+"Porovnávanie s najlepšími podnikmi",
+"Kontrola daní",
+"Výpočet miezd",
+"Predaj produktov"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je MBO?",
+options: [
+"Riadenie pomocou cieľov",
+"Daňová kontrola",
+"Marketingový plán",
+"Výpočet nákladov"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo robí manažér ako monitor?",
+options: [
+"Sleduje informácie a prostredie",
+"Predáva produkty",
+"Kontroluje mzdy",
+"Vyjednáva zmluvy"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo robí manažér ako hovorca?",
+options: [
+"Komunikuje s externým prostredím",
+"Kontroluje výrobu",
+"Počíta dane",
+"Vedie sklad"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je motivácia?",
+options: [
+"Podnecovanie pracovníkov k výkonu",
+"Kontrola výroby",
+"Daňový systém",
+"Forma konkurencie"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo sú interpersonálne schopnosti manažéra?",
+options: [
+"Schopnosť komunikácie s ľuďmi",
+"Výpočet daní",
+"Predaj výrobkov",
+"Riadenie exportu"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo sú koncepčné schopnosti?",
+options: [
+"Schopnosť chápať súvislosti",
+"Výpočet miezd",
+"Predaj služieb",
+"Kontrola výroby"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je podnik?",
+options: [
+"Ekonomicky samostatná jednotka",
+"Štátny úrad",
+"Výrobný faktor",
+"Typ dane"
+],
+correctAnswer: 0
+},
+
+{
+question: "Ktoré znaky má podnik?",
+options: [
+"Právna subjektivita a ekonomická samostatnosť",
+"Inflácia a export",
+"Deflácia a import",
+"Menová politika"
+],
+correctAnswer: 0
+},
+
+{
+question: "Ktoré podniky patria do terciárneho sektora?",
+options: [
+"Poisťovne a obchodné podniky",
+"Poľnohospodárske podniky",
+"Ťažobné podniky",
+"Energetické podniky"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je mikroprostredie podniku?",
+options: [
+"Faktory blízke podniku",
+"Globálna ekonomika",
+"Daňový systém",
+"Menová politika"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je makroprostredie?",
+options: [
+"Vonkajšie ekonomické faktory",
+"Interná štruktúra firmy",
+"Výrobný proces",
+"Účtovníctvo"
+],
+correctAnswer: 0
+},
+
+{
+question: "Ktorý faktor patrí do makroprostredia?",
+options: [
+"Demografické prostredie",
+"Sklad podniku",
+"Mzdy pracovníkov",
+"Výrobná hala"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo znamená internacionalizácia?",
+options: [
+"Prepojenie ekonomík medzi krajinami",
+"Pokles exportu",
+"Likvidácia podnikov",
+"Rast inflácie"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo znamená ekologizácia?",
+options: [
+"Snaha o ochranu životného prostredia",
+"Rast exportu",
+"Zvyšovanie cien",
+"Daňová reforma"
+],
+correctAnswer: 0
+},
+
+{
+question: "Ktoré fázy má životný cyklus podniku?",
+options: [
+"Vznik, rast, stabilizácia, kríza, zánik",
+"Export, import, inflácia",
+"Výroba, predaj, marketing",
+"Kontrola, audit, predaj"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je podnikateľský zámer?",
+options: [
+"Zjednodušená stratégia podniku",
+"Daňové priznanie",
+"Mzda zamestnanca",
+"Úverová zmluva"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je interný rast podniku?",
+options: [
+"Rast pomocou vlastných zdrojov",
+"Fúzia podnikov",
+"Predaj majetku",
+"Znižovanie výroby"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je externý rast podniku?",
+options: [
+"Rast pomocou externých zdrojov",
+"Kontrola výroby",
+"Pokles exportu",
+"Znižovanie nákladov"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je kríza likvidity?",
+options: [
+"Neschopnosť splácať záväzky",
+"Rast exportu",
+"Vyššia produktivita",
+"Rast zisku"
+],
+correctAnswer: 0
+},
+
+{
+question: "Čo je kríza rentability?",
+options: [
+"Nedostatočný výnos podniku",
+"Rast cien akcií",
+"Vyšší export",
+"Pokles nákladov"
+],
+correctAnswer: 0
+}
+
+];
+
+function shuffleArray(array) {
+
+for (let i = array.length - 1; i > 0; i--) {
+
+const j = Math.floor(Math.random() * (i + 1));
+
+[array[i], array[j]] = [array[j], array[i]];
+}
+
+return array;
+}
+
+questions.forEach(q => {
+
+const correct = q.options[q.correctAnswer];
+
+q.options = shuffleArray(q.options);
+
+q.correctAnswer = q.options.indexOf(correct);
+
+});
+
+shuffleArray(questions);
+
+window.questions = questions;
+
+console.log("Questions loaded:", questions.length);
 question: "Čo je agregátny dopyt?",
 options: [
 "Celkové výdavky ekonomiky",
